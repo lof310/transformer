7feefc78fe083cdfd0294ceaf1e4088e38cc2ec7
lof310 <lof310w@gmail.com>
2026-03-09 23:54:29 -0400

    add utils.py

---
+++ transformer/utils.py
@@ -0,0 +1,18 @@
+import math
+import os
+import random
+import sys
+
+from typing import Dict, Optional, Tuple, Type, Union
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+def check_type(x: Union[Type[nn.Module], str]):
+    if isinstance(x, str):
+        return 0
+    elif isinstance(x, type) and issubclass(x, nn.Module):
+        return 1
+    else:
+        TypeError("Type not valid")
