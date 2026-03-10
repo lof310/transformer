7c69c25805cbfb96b960488e1a3dc5e3d61150e1
lof310 <lof310w@gmail.com>
2026-03-09 23:46:29 -0400

    update config.py

--- transformer/config.py
+++ transformer/config.py
@@ -29,6 +29,14 @@ class TransformerConfig(PretrainedConfig):
 
         attn_qk_norm (bool, optional): Whether to apply Normalization to Queries and Keys before the Attention Computation. Default: ``True``
 
+        norm_type (Union[Type[nn.Module], str], optional): Normalization class or type.
+            - If ``str``, one of ``rms_norm`` or ``layer_norm``.
+            - If ``Type[nn.Module]`` then will be instantiated inside the model.
+              Should have the same API as a torch Normalization Layer.
+              Default: ``rms_norm``
+
+        norm_design (str, optional): Normalization Design, one of ``pre-norm``, ``post-norm`` or ``both``. Default: ``pre-norm``
+
         attn_dropout (float, optional): Dropout probability for the Attention Layer. Default: ``0.0``
 
         ffn_bias (bool, optional): Whether to use bias in Feed-Forward Linear layers. Default: ``True``
@@ -39,9 +47,16 @@ class TransformerConfig(PretrainedConfig):
 
         rope_base (float, optional): Base for the RoPE frequency computation. Default: ``10000.0``
 
-        attn_type (str, optional): Attention type, one of ``MHA``, ``GQA``, ``CrossAttention``. For ``GQA``, also specify `n_kv_heads`. Default: ``MHA``
+        attn_type (Union[Type[nn.Module], str], optional): Attention class or type.
+            - If ``str``, one of ``MHA``, ``GQA``, ``CrossAttention``. For ``GQA``, also specify `n_kv_heads`.
+            - If ``Type`` then will beinstantiated inside the model.
+              Should have the same API as ``transformer.attn.MHA``.
+              Default ``MHA``
 
         n_kv_heads (int, optional): Number of key/value heads for Grouped-Query Attention(GQA). Default: ``n_heads``
+
+        pos_encoding (str, optional): Positional Encoding for attention, one of ``RoPE``, ``AliBI``, ``PartialRoPE``. Default: ``RoPE``
+            Note: Is recommended to change the default to ``PartialRoPE`` which is used in SOTA models like Qwen3-Next-80B-A3B
 
         rope_base (float, optinal): Base for the Exponential Frequency Calculation in RoPE. Default: ``10000.0``
 
@@ -54,17 +69,19 @@ class TransformerConfig(PretrainedConfig):
     def __init__(
         self,
         n_layers: int = 12,
+        d_model: int = 1536,
         n_heads: int = 32,
         n_kv_heads: Optional[int] = None,
         vocab_size: int = 50000,
-        d_model: int = 1536,
         d_ff: Optional[int] = None,
-        attn_type: str = "MHA",
+        norm_design: str = "pre_norm",
+        norm_type: Union[Type[nn.Module], str] = "rms_norm",
+        attn_type: Union[Type[nn.Module], str] = "MHA",
         attn_bias: Optional[bool] = False,
-        attn_dropout: Optional[float] = 0.0,
         ffn_bias: bool = True,
-        attn_qk_norm: bool = True,
         lm_head_bias: bool = False,
+        attn_qk_norm: bool = True,
+        attn_dropout: Optional[float] = 0.0,
         tied_weights: bool = False,
         seq_len: int = 1024,
         rope_base: float = 10000.0,
@@ -72,21 +89,28 @@ class TransformerConfig(PretrainedConfig):
         **kwargs: Dict,
     ):
         super().__init__(**kwargs)
+
         self.n_layer = n_layers
+        self.d_model = d_model
         self.n_heads = n_heads
-        self.attn_type = attn_type
         self.n_kv_heads = n_kv_heads if attn_type == "GQA" else n_heads
         self.vocab_size = vocab_size
-        self.attn_bias = attn_bias
+
+        self.attn_type = attn_type
+        self.norm_type = norm_type
+
+        self.d_ff = d_ff if d_ff is not None else math.ceil(d_model * 2.666)
+
         self.attn_dropout = attn_dropout
-        self.ffn_bias = ffn_bias
         self.attn_qk_norm = attn_qk_norm
+
+        self.attn_bias = attn_bias
+        self.ffn_bias = ffn_bias
         self.lm_head_bias = lm_head_bias
+
         self.tied_weights = tied_weights
 
         self.seq_len = seq_len
         self.rope_base = rope_base
+        self.pos_encoding = pos_encoding
         self.max_seq_len = max_seq_len
-
-        self.d_model = d_model
-        self.d_ff = d_ff if d_ff is not None else math.ceil(d_model * 2.666)
